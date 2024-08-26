ce9a4f9ae3709d2e28e0de52e321b7e8326dd870
empchief <bjornmagnekristensen1999@gmail.com>
2024-08-26 16:25:25 +0200

    Finished speller from pset5

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -2,7 +2,10 @@
 
 #include <ctype.h>
 #include <stdbool.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
 #include "dictionary.h"
 
 // Represents a node in a hash table
@@ -18,10 +21,26 @@ const unsigned int N = 26;
 // Hash table
 node *table[N];
 
+// FUnksjona
+unsigned int hash_value;
+unsigned int word_count;
+
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
     // TODO
+    // ta inn input og skriv et tall til den tilsvarende bøtte
+    // gå gjennom listen
+    hash_value = hash(word);
+    node *cursor = table[hash_value];
+    while (cursor != 0)
+    {
+        if (strcasecmp(cursor->word, word) == 0)
+        {
+            return true;
+        }
+        cursor = cursor->next;
+    }
     return false;
 }
 
@@ -29,26 +48,74 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    // Gjør hash funksjonen bedre
+    unsigned long total = 0;
+    for (int index = 0; index < strlen(word); index++)
+    {
+        total += tolower(word[index]);
+    }
+    return total % N;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
+// åpne ordboken og returner null, les strenger, opprett en ny node, hash ordet, sett inn node
+// fscan hvis slutten av filen nås, while-løkke for å opprette node, malloc av minnestørrelse
+// hvis ikke åpnes, NULL
+// Opprett en variabel for ord
+// skann ordboken for strenger til EOF
+// malloc eller returner false, kopier 2 noder
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        printf("Could not open %s.\n", dictionary);
+        return false;
+    }
+    char word[LENGTH + 1];
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        node *cursor = malloc(sizeof(node));
+        if (cursor == NULL)
+        {
+            return false;
+        }
+        strcpy(cursor->word, word);
+        hash_value = hash(word);
+        cursor->next = table[hash_value];
+        table[hash_value] = cursor;
+        word_count++;
+    }
+    fclose(file);
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
+// retuner ord telling eller retuner 0
 unsigned int size(void)
 {
-    // TODO
+    if (word_count > 0)
+    {
+        return word_count;
+    }
     return 0;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
+// lag en temp-variabel som peker til første node
+// gjør fri temp-pekeren etter å ha flyttet peker (cursor) og frigjør temp
+// frigjør dynamisk allokert minne
 bool unload(void)
 {
-    // TODO
-    return false;
+    for (int index = 0; index < N; index++)
+    {
+        node *cursor = table[index];
+        while (cursor != 0)
+        {
+            node *temp = cursor;
+            cursor = cursor->next;
+            free(temp);
+        }
+    }
+    return true;
 }
